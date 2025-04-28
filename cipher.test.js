617a7de13bc6a7bd3b43480c201c6f1c6baa8cf8
Zak <zak.hajjaji@icloud.com>
2025-04-28 15:00:05 +0100

    write new failing tests

--- cipher.test.js
+++ cipher.test.js
@@ -1,5 +1,5 @@
 const cipher = require('./cipher.js');
 
 test('decodes a simple message with a key of 1', () => {
-    expect(cipher('BCD', 1, 'decode')).toBe('ABC');
+    expect(cipher('EFG', 1, 'decode')).toBe('abc');
   });
